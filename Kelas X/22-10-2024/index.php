ae69c42f4691939b6f98d7c1bf7aaa934eb71f9d
Oktavian-X-RPL <oktavianwibisono90@gmail.com>
2024-10-24 22:50:13 +0700

    Add files via upload

---
+++ Kelas X/22-10-2024/index.php
@@ -0,0 +1,40 @@
+\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .kamar {
+            text-align: center;
+        }
+        .list {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="kamar">
+    <h1>Belajar PHP</h1>
+    <p>Hari ini saya belajar PHP</p>
+    <h2>Materi PHP</h2>
+    <div class="list">
+    <ol>
+        <li>Variabel</li>
+        <p>Variabel adalah wadah atau tempat untuk menyimpan data.</p>
+        <p>Data bisa berupa text atau string, bisa juga berupa angka atau numerik, data juga bisa gabungan text, angka dan simbol.</p>
+        <li>Array</li>
+        <li>Pengujian</li>
+        <li>Pengulangan</li>
+        <li>Function</li>
+        <li>Class</li>
+        <li>Object</li>
+        <li>Framework</li>
+        <li>PHP dan MySQL</li>
+    </ol>
+    </div>
+    </div>
+</body>
+</html>
